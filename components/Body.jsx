9088899a636b3a573e505f6a99f65a2915e3cc45
TienLe <trantiendev@gmail.com>
2022-05-21 03:50:09 +0700

    nextAuth spotify && signin cpn session

--- components/Body.jsx
+++ components/Body.jsx
@@ -7,6 +7,9 @@ const Body = () => {
   return (
     <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
       <Search search={search} setSearch={setSearch} />
+      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 overflow-y-scroll scrollbar-hide h-96 p-4 gap-x-4 gap-y-8">
+
+      </div>
     </section>
   )
 }
---
+++ pages/auth/signin.js
@@ -0,0 +1,46 @@
+import { getProviders, signIn } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+
+const Signin = ({ providers }) => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  console.log(session, status);
+
+  useEffect(() => {
+    session && router.push('/');
+  }, [session]);
+
+  return (
+    <div className="bg-black h-screen flex flex-col items-center pt-40 space-y-8">
+      <Head>
+        <title>Login - Spotify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <img src="https://rb.gy/y9mwtb" className="h-[250px] w-[600px] animate-pulse object-contain" alt="logo" />
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="text-white py-4 px-6 rounded-full bg-[#1db954] transition duration-300 ease-out border border-transparent uppercase font-bold text-xs md:text-base tracking-wider hover:scale-105 hover:bg-[#0db146]"
+            onClick={() => signIn(provider.id)}
+          >
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Signin;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
